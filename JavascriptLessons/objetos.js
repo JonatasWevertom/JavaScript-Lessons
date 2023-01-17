d478782ff211763cf56acc1d83a2900c925a4490
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-17 17:54:39 -0300

    adição de comentarios ao arquivo

--- JavascriptLessons/objetos.js
+++ JavascriptLessons/objetos.js
@@ -1,3 +1,4 @@
+// Exemplo de objeto com chave e valor
 let pessoa = {
     nome: 'Jonatas',
     idade: 27,
@@ -5,8 +6,10 @@ let pessoa = {
     sobrenome: 'Araujo',
 };
 
+// exemplo de saida no console para o objeto e suas chaves exibindo o valor dela
 console.log(pessoa);
 console.log(pessoa.nome);
 
+// exemplo de saida no console para exibir o tipo do objeto e de sua chave
 console.log(typeof (pessoa));
 console.log(typeof (pessoa.nome));
