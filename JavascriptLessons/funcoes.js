7e04d1b03d99cba1807b3068bd28a7b51ae5194a
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-17 17:53:40 -0300

    funcoes

---
+++ JavascriptLessons/funcoes.js
@@ -0,0 +1,20 @@
+// Tipo de função que nao tem retorno, quando utilizada com objetos chamasse atributo
+function dizerNome() {
+    console.log('Jonatas');
+};
+
+dizerNome();
+
+// Tipo de função que nao tem retorno, quando utilizada com objetos chamasse metodo
+function multiplicarPorDois(num) {
+    return num * 2;
+};
+
+// tipo de saida via console que exibe chamando diretamente a função e passando o parametro.
+console.log(multiplicarPorDois(5));
+
+// atribuição de uma funcao a uma variavel para usar repetidas vezes
+let resultado = multiplicarPorDois(5);
+
+// saida via console simplificada usando variavel
+console.log(resultado);
