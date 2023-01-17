d4880a38f85b7cf706b83c617de6581cd4055ca9
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-17 17:18:59 -0300

    objetos

---
+++ JavascriptLessons/objetos.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    nome: 'Jonatas',
+    idade: 27,
+    estaAprovado: true,
+    sobrenome: 'Araujo',
+};
+
+console.log(pessoa);
+console.log(pessoa.nome);
+
+console.log(typeof (pessoa));
+console.log(typeof (pessoa.nome));
