1e5e8fb9ef3db7597df4f43ac21b39636707a05f
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-17 18:21:38 -0300

    primeiro projeto

---
+++ projeto1/scripts.js
@@ -0,0 +1,10 @@
+// utilização de variavel auxiliar para troca de valores entre 2 variaveis.
+let a = 'vermelho';
+let b = 'azul';
+let aux = a;
+
+a = b;
+b = aux;
+
+console.log(a)
+console.log(b)
