1d11165a92c75fa1a413233cdd5f2dc2d42aba8f
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-17 18:04:12 -0300

    ternario

---
+++ JavascriptLessons/ternario.js
@@ -0,0 +1,8 @@
+// Exemplo usando abastecimento com gasolina premium ou comum usando operador ternario
+
+// let pontos = 200;
+let pontos = 100;
+
+let tipo = pontos > 100 ? 'premium' : 'comum';
+
+console.log(tipo);
