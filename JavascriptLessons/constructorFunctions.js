c06bc5ef2c03f43ecfd5f304a3e1b5e1beb2217e
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-18 23:58:51 -0300

    Constructor Functions

---
+++ JavascriptLessons/constructorFunctions.js
@@ -0,0 +1,23 @@
+function criarCelular(marca, tamanhoTela, capacidadeBateria) {
+    return {
+        marca,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar() {
+            console.log('Fazendo ligação...');
+        },
+    };
+};
+
+function Celular(marca, tamanhoTela, capacidadeBateria) {
+    this.marca = marca,
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function(){
+        console.log('Fazendo Ligação...');
+    };
+};
+
+const celular1 = new Celular('Apple', 7, 6000);
+
+console.log(celular1);
