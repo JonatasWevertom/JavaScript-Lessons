83e7ed5268f6075eb68f653cd0a673a746fdfd9b
Jonatas Weverton <jonataswevertom@hotmail.com>
2023-01-20 01:18:41 -0300

    projeto 12

---
+++ projeto12/scripts.js
@@ -0,0 +1,26 @@
+function Endereco(rua, cidade, cep) {
+    this.rua = rua,
+    this.cidade = cidade,
+    this.cep = cep
+};
+
+const endereco1 = new Endereco('Av. Bernardo vieira', 'Natal', '55555-555');
+const endereco2 = new Endereco('Av. Bernardo vieira', 'Natal', '55555-555');
+
+function saoIguais(endereco1, endereco2) {
+    // comparar se as propriedades são iguais
+
+    for (let chave in endereco1) {
+        return endereco1[chave] === endereco2[chave];
+    };
+};
+
+function temEnderecoMemoriaIguais(endereco1, endereco2) {
+    // comparar se a referencia do objeto aponta para o mesmo local na memória 
+
+    return endereco1 === endereco2;
+    
+};
+
+console.log(saoIguais(endereco1, endereco2));
+console.log(temEnderecoMemoriaIguais(endereco1, endereco2));
